831609cca0c5451bbe7f912d5b1b6ce07f3811b9
Wakaba <w@suika.fam.cx>
2011-07-16 16:24:26 +0900

    Chrome Web Store sucks...

--- chrome/pack-hatena-star-everywhere.bat
+++ chrome/pack-hatena-star-everywhere.bat
@@ -1,4 +1,13 @@
 del hatena-star-everywhere.crx
 del hatena-star-everywhere.zip
 "%USERPROFILE%\Local Settings\Application Data\Google\Chrome\Application\chrome.exe" --pack-extension="%CD%\hatena-star-everywhere" --pack-extension-key="%CD%\hatena-star-everywhere.pem"
-zip hatena-star-everywhere.zip hatena-star-everywhere -r
+mkdir tmp
+cp -R hatena-star-everywhere tmp/hatena-star-everywhere
+perl -e "local $/ = undef; my $s = <>; $s =~ s{\x22update_url\x22\s*:\s*\x22[^\x22]*\x22\s*,\s*}{}; $s =~ s{(\x22[^\x22]*\x22)|/\*.*?\*/}{$1}gs; print $s" < hatena-star-everywhere\manifest.json > tmp\hatena-star-everywhere\manifest.json
+perl -e "local $/ = undef; my $s = <>; $s =~ s{\x22update_url\x22\s*:\s*\x22[^\x22]*\x22\s*,\s*}{}; $s =~ s{(\x22[^\x22]*\x22)|/\*.*?\*/}{$1}gs; print $s" < hatena-star-everywhere\_locales\ja\messages.json > tmp\hatena-star-everywhere\_locales\ja\messages.json
+perl -e "local $/ = undef; my $s = <>; $s =~ s{\x22update_url\x22\s*:\s*\x22[^\x22]*\x22\s*,\s*}{}; $s =~ s{(\x22[^\x22]*\x22)|/\*.*?\*/}{$1}gs; print $s" < hatena-star-everywhere\_locales\en\messages.json > tmp\hatena-star-everywhere\_locales\en\messages.json
+perl -e "local $/ = undef; my $s = <>; $s =~ s{\x22update_url\x22\s*:\s*\x22[^\x22]*\x22\s*,\s*}{}; $s =~ s{(\x22[^\x22]*\x22)|/\*.*?\*/}{$1}gs; print $s" < hatena-star-everywhere\_locales\en_GB\messages.json > tmp\hatena-star-everywhere\_locales\en_GB\messages.json
+cd tmp
+zip ../hatena-star-everywhere.zip hatena-star-everywhere -r
+cd ..
+rm -fr ./tmp
