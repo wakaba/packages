c663c38e3eff9b490877b8089cf29b7de275ea39
Wakaba <w@suika.fam.cx>
2011-07-16 15:06:13 +0900

    Generate zip

--- chrome/pack-hatena-star-everywhere.bat
+++ chrome/pack-hatena-star-everywhere.bat
@@ -1,1 +1,4 @@
-"%USERPROFILE%\Local Settings\Application Data\Google\Chrome\Application\chrome.exe" --pack-extension="%CD%\hatena-star-everywhere" --pack-extension-key="%CD%\hatena-star-everywhere.pem" --no-message-box
+del hatena-star-everywhere.crx
+del hatena-star-everywhere.zip
+"%USERPROFILE%\Local Settings\Application Data\Google\Chrome\Application\chrome.exe" --pack-extension="%CD%\hatena-star-everywhere" --pack-extension-key="%CD%\hatena-star-everywhere.pem"
+zip hatena-star-everywhere.zip hatena-star-everywhere -r
